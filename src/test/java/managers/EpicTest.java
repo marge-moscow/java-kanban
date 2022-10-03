9d659243cb010b480105152399d47c07b5125b00
MargeMoscow <margemoscow@yandex.ru>
2022-10-04 00:52:40 +0300

    style: изменить оформление кода в классе EpicTest

--- src/test/java/managers/EpicTest.java
+++ src/test/java/managers/EpicTest.java
@@ -6,7 +6,6 @@ import model.Epic;
 import model.Subtask;
 import model.TaskStatus;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,14 +16,10 @@ public class EpicTest {
     protected Epic epic;
     protected Subtask subtask1;
     protected Subtask subtask2;
-    protected Subtask subtask3;
     protected TaskManger manager;
-
     protected int epicId;
-
     protected TaskStatus actual;
     protected TaskStatus expected;
-
 
     @BeforeEach
     public void init () {
@@ -36,18 +31,14 @@ public class EpicTest {
 
         subtask1 = new Subtask();
         subtask2 = new Subtask();
-        subtask3 = new Subtask();
-
 
         subtask1.setEpicId(epicId);
         subtask2.setEpicId(epicId);
-        subtask3.setEpicId(epicId);
-
+        
     }
 
     @Test
     public void testEpicStatusEmptySubtaskList() {
-
         expected = TaskStatus.NEW;
         manager.checkEpicStatus(epicId);
         actual = epic.getStatus();
@@ -58,67 +49,52 @@ public class EpicTest {
 
     @Test
     public void testEpicStatusAllSubtasksNew() {
-
         subtask1.setStatus(TaskStatus.NEW);
         manager.addItem(subtask1);
-
         expected = TaskStatus.NEW;
         manager.checkEpicStatus(epicId);
         actual = epic.getStatus();
 
-        assertEquals(expected, actual, "Сатус не совпадает.");
+        assertEquals(expected, actual, "Статус не совпадает.");
 
     }
 
     @Test
     public void testEpicStatusAllSubtasksDone() {
-
         subtask1.setStatus(TaskStatus.DONE);
         manager.addItem(subtask1);
-
         expected = TaskStatus.DONE;
         manager.checkEpicStatus(epicId);
         actual = epic.getStatus();
 
-        assertEquals(expected, actual, "Сатус не совпадает.");
+        assertEquals(expected, actual, "Статус не совпадает.");
 
     }
 
     @Test
     public void testEpicStatusSubtasksNewAndDone() {
-
         subtask1.setStatus(TaskStatus.NEW);
         subtask2.setStatus(TaskStatus.DONE);
         manager.addItem(subtask1);
         manager.addItem(subtask2);
-
         expected = TaskStatus.IN_PROGRESS;
         manager.checkEpicStatus(epicId);
         actual = epic.getStatus();
 
-        assertEquals(expected, actual, "Сатус не совпадает.");
+        assertEquals(expected, actual, "Статус не совпадает.");
 
     }
 
     @Test
     public void testEpicStatusAllSubtasksInProgress() {
-
         subtask1.setStatus(TaskStatus.IN_PROGRESS);
         manager.addItem(subtask1);
-
         expected = TaskStatus.IN_PROGRESS;
         manager.checkEpicStatus(epicId);
         actual = epic.getStatus();
 
-        assertEquals(expected, actual, "Сатус не совпадает.");
+        assertEquals(expected, actual, "Статус не совпадает.");
 
     }
-
-
-
-
-
-
-
 
 }
